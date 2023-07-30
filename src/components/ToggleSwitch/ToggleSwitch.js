7d44d85f894e35fc1cfeb6f1974723be6b73b8d0
BryanAnjola <heyitsbryxn@gmail.com>
2023-07-29 18:29:18 -0600

    project 11

--- src/components/ToggleSwitch/ToggleSwitch.js
+++ src/components/ToggleSwitch/ToggleSwitch.js
@@ -1,6 +1,6 @@
 import React, { useContext, useState } from "react";
 import "./ToggleSwitch.css";
-import { currentTemperatureUnitContext } from "../../Contexts/CurrentTempetureUnit";
+import { currentTemperatureUnitContext } from "../../contexts/CurrentTempetureUnit";
 
 const ToggleSwitch = () => {
   // const [currentTemperatureUnit, handleToggleSwitchChange] = useState("C");
